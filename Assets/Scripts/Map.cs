96728d8d6d921f3679107367bcc2ca6cf18b8293
VDeltaGabriel <gabrielvdelta@gmail.com>
2025-04-24 17:30:44 +0200

    Map WIP

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,30 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using Random = UnityEngine.Random;
 
 public class Map : MonoBehaviour
 {
-    public GameObject point;
+    public GameObject player;
+    public MapNode point;
     public UILine line;
-    public float paths = 2;
+    public float nPaths = 2;
     public int rows = 1;
     public int columns = 1;
+
+    private GameObject plr;
     
     private RectTransform _tr;
     private Bounds _bounds;
 
     private Transform _prevPoint = null;
+
+    struct Node
+    {
+        public Vector2 mapPos;
+        public int gridPos;
+        public MapNode mapNode;
+    }
     
     private void Awake()
     {
+        plr = Instantiate(player, transform);
+        plr.SetActive(false);
         _bounds = GetMapArea();
         Debug.Log($"Bounds: {_bounds} | {_bounds.min} {_bounds.max}");
         
         float width = _bounds.max.x - _bounds.min.x, height = _bounds.max.y - _bounds.min.y;
         float spacingX = width / columns, spacingY = height / rows;
         
+        Vector2[] grid = new Vector2[rows*columns];
         for (int r = 0; r < rows; r++)
         {
            
@@ -32,19 +47,112 @@ public class Map : MonoBehaviour
             {
                 int idx = r * columns + c;
                 Vector2 pos = new Vector2(_bounds.min.x + spacingX * c + spacingX / 2f, _bounds.min.y + spacingY * r + spacingY / 2f);
-                //_grid[idx] = pos;
-                GameObject obj = Instantiate(point, transform);
-                obj.transform.localPosition = pos;  
+                grid[idx] = pos;
             }
         }
+
+        List<List<Node>> paths = new List<List<Node>>();
+        List<Node> rnodes = new List<Node>();
+        List<Vector2> nodes = new List<Vector2>();
         
+        for (int i = 0; i < nPaths; i++)
+        {
+            List<Node> path = new List<Node>();
+            int prevNode = -1;
+            for (int r = 0; r < rows; r++)
+            {
+                if (r + 1 >= rows) break;
+                int node = 0;
+                if (r == 0)
+                {
+                    node = Random.Range(0, columns);
+                }
+                if(prevNode != -1)
+                {
+                    node = prevNode;
+                }
+                int idx = r * columns + node;
+                List<int> neighbours = new List<int>();
+                List<int> neighboursRawIdx = new List<int>();
+                neighbours.Add((r + 1) * columns + node);
+                neighboursRawIdx.Add(node);
+                if (node - 1 >= 0)
+                {
+                    neighbours.Add((r + 1) * columns + node - 1);
+                    neighboursRawIdx.Add(node-1);
+                }
+
+                if (node + 1 < rows)
+                {
+                    neighbours.Add((r + 1) * columns + node + 1);
+                    neighboursRawIdx.Add(node+1);
+                }
+                
+                int selection = Random.Range(0, neighbours.Count);
+                prevNode = neighboursRawIdx[selection];
+                Node n = new Node();
+                GameObject line = AddLine(grid[idx], grid[neighbours[selection]], Color.white);
+                
+                if (!nodes.Contains(grid[idx]))
+                {
+                    nodes.Add(grid[idx]);
+                    MapNode m = AddPoint(grid[idx]);
+                    m.AddEvent(() =>
+                    {
+                        plr.transform.localPosition = grid[neighbours[selection]];
+                        m.Interactable = false;
+                        Destroy(line);
+                    });
+                    n.gridPos = idx;
+                    n.mapPos = grid[idx];
+                    n.mapNode = m;
+                    path.Add(n);
+                    rnodes.Add(n);
+                }
+                else
+                {
+                    MapNode m = rnodes.Find(x => x.gridPos == idx).mapNode;
+                    m.AddEvent(() =>
+                    {
+                        plr.transform.localPosition = grid[neighbours[selection]];
+                        m.Interactable = false;
+                        Destroy(line);
+                    });
+                }
+                if (!nodes.Contains(grid[neighbours[selection]]))
+                {
+                    nodes.Add(grid[neighbours[selection]]);
+                    MapNode m = AddPoint(grid[neighbours[selection]]);
+                    n.gridPos = neighbours[selection];
+                    n.mapPos = grid[neighbours[selection]];
+                    n.mapNode = m;
+                    path.Add(n);
+                }
+            }
+            paths.Add(path);
+        }
+        
+        int chosenPath = Random.Range(0, paths.Count);
+        Node startNode = paths[chosenPath][0];
+        startNode.mapNode.NodeType = NodeType.StartNode;
+        plr.transform.localPosition = startNode.mapPos;
+        plr.SetActive(true);
     }
 
-    private void AddLine(Vector2 start, Vector2 end)
+    private MapNode AddPoint(Vector2 pos)
+    {
+        MapNode obj = Instantiate(point, transform);
+        obj.transform.localPosition = pos;
+        return obj;
+    }
+    
+    private GameObject AddLine(Vector2 start, Vector2 end, Color color)
     {
         UILine obj = Instantiate(line, transform);
+        obj.color = color;
         obj.start = start;
         obj.end = end;
+        return obj.gameObject;
     }
 
     private Bounds GetMapArea()
---
+++ Assets/Scripts/MapNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public enum NodeType
+{
+    StartNode,
+    EncounterNode,
+    EndNode
+}
+
+[DisallowMultipleComponent]
+public class MapNode : MonoBehaviour
+{
+    private NodeType _nodeType;
+    private Button _button;
+
+    public NodeType NodeType
+    {
+        get => _nodeType;
+        set
+        {
+            if (value == NodeType.StartNode) Interactable = false;
+            _nodeType = value;
+        }
+
+    }
+
+    public bool Interactable
+    {
+        get => _button.interactable;
+        set => _button.interactable = value;
+    }
+
+    public void AddEvent(UnityAction ev)
+    {
+        _button.onClick.AddListener(ev);
+    }
+    
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+}
